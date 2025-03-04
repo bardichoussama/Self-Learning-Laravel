4a9184553627e463c1ca1cd266aa719842a2c309
Oussama Bardich <98558799+bardichoussama@users.noreply.github.com>
2025-03-04 13:53:59 +0000

    applay service into prototype

--- laravel-vuejs-crud-prototype/app/Http/Requests/StoreProductRequest.php
+++ laravel-vuejs-crud-prototype/app/Http/Requests/StoreProductRequest.php
@@ -6,22 +6,13 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreProductRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
+
     public function authorize()
     {
-        // Return true to allow all users to make this request
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
+
     public function rules()
     {
         return [
